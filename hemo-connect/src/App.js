16d3bc62a8feb500d98fde4a033f5a4b5122e79e
AKSHITHBILLA14 <billasaiakshith@gmail.com>
2025-03-08 19:56:17 +0530

    hemo-connect

--- hemo-connect/src/App.js
+++ hemo-connect/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/homepage";
+import Login from "./components/login";
+import HelpRequest from "./components/helpReq";
+import NotificationPopup from "./components/NotificationPopUP";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          
+          <Route path="/" element={<Login/>} />
+
+          
+          <Route path="/help" element={<HelpRequest />} />
+
+          
+          <Route path="/notifications" element={<NotificationPopup />} />
+          
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
